0f18c4d8b125dc8c43172e3709b1f9d7e21d2deb
fvn70 <fvn_reg@mail.ru>
2022-04-23 09:19:54 +0700

    Stage 3/4: History

--- Magic Form/task/src/script.js
+++ Magic Form/task/src/script.js
@@ -1,27 +1,25 @@
-let formData = JSON.parse(localStorage.getItem('formData')) || [];
+let formData = [];
 let fields = document.querySelectorAll('input');
+const saveBtn = document.getElementById('submit-button');
+const form = document.getElementById('submit-form');
 
-fields.forEach((item) => {
-    console.log(item.innerHTML);
-    item.addEventListener('change', saveData);
-});
-
-loadData();
+form.addEventListener('submit', e => { e.preventDefault(); });
+saveBtn.addEventListener('click', saveData);
 
 function saveData() {
-    formData = [];
-    for (let i = 0; i < fields.length; i++) {
-       formData[i] = fields[i].value;
+    formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let data = [];
+    let k = formData.length;
+    let id = 0;
+    if (k > 0) {
+        id = formData[k - 1][0];
     }
+    data[0] = id + 1;
+    for (let i = 0; i < fields.length; i++) {
+       data[i + 1] = fields[i].value;
+    }
+    formData[k] = data;
     localStorage.setItem('formData', JSON.stringify(formData));
+    form.reset();
 }
 
-function loadData() {
-    if (formData) {
-        for (let i = 0; i < fields.length; i++) {
-            if (formData[i]) {
-                fields[i].value = formData[i];
-            }
-        }
-    }
-}
